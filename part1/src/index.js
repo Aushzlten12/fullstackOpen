e73165fea3554e2b1489a439ffc6642d57842d31
Aushzlten12 <josepachas1604@gmail.com>
2023-11-11 01:38:40 -0500

    Paused

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => ( 
-  <div>
-    <p>Hello world</p> 
-  </div>
-)
+const App = () => { 
+  console.log('Hello from component')
+  const now = new Date()
+  const a = 10
+  const b = 20
+
+  return (
+    <div>
+      <p>Hello world, it is {now.toString()}</p>
+      <p>
+        {a} plus {b} is {a+b}
+      </p> 
+    </div>
+  )
+} 
 
 ReactDOM.render(<App />, document.getElementById('root')) 
+
+// Continue JSX

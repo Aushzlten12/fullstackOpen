20de01c29f1d60974a006a32d1df8a4468df94e2
Aushzlten12 <josepachas1604@gmail.com>
2023-11-25 21:53:50 -0500

    Exercise 1.6 finished now

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,19 +1,38 @@
-import React, { useState } from 'react'
-import ReactDOM from 'react-dom'
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+const Title = ({ text }) => (
+  <h1>{text}</h1>
+);
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
+
+const Statics = ({ good, neutral, bad }) => (
+  <div>
+    <p>good {good}</p>
+    <p>neutral {neutral}</p>
+    <p>bad {bad}</p>
+  </div>
+);
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   return (
     <div>
-      code here
+      <Title text={"give feedback"} />
+      <Button handleClick={() => setGood(good + 1)} text={"Good"} />
+      <Button handleClick={() => setNeutral(neutral + 1)} text={"Neutral"} />
+      <Button handleClick={() => setBad(bad + 1)} text={"Bad"} />
+      <Title text={"statics"} />
+      <Statics good={good} neutral={neutral} bad={bad} />
     </div>
-  )
-}
+  );
+};
 
-ReactDOM.render(<App />, 
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById("root"));

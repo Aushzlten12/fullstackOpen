05fda8eab0a5f322c51e869fbc9605d5c2ae5d87
Aushzlten12 <josepachas1604@gmail.com>
2023-12-23 11:23:56 -0500

    Exercise 2.2 finished

--- part2/infocourse/src/components/Content.js
+++ part2/infocourse/src/components/Content.js
@@ -2,11 +2,13 @@ import React from "react";
 import Part from "./Part";
 
 const Content = ({ parts }) => {
+  const totalExercises = parts.reduce((s, p) => s + p.exercises, 0);
   return (
     <div>
       {parts.map((part) => {
         return <Part key={part.id} part={part} />;
       })}
+      <p>Total of {totalExercises} exercises</p>
     </div>
   );
 };

98f8873500049971084f56de31fd3d4c90c21b46
Aushzlten12 <josepachas1604@gmail.com>
2023-12-26 21:31:06 -0500

    Exercise 2.14 completed I used OpenWeather not include pictures

--- part2/searchcountry/src/components/Country.js
+++ part2/searchcountry/src/components/Country.js
@@ -1,6 +1,23 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const CountryInformation = ({ country }) => {
+  const [weather, setWeather] = useState({});
+
+  useEffect(() => {
+    const api_key = process.env.REACT_APP_API_KEY;
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${country.capital}&appid=${api_key}&units=metric`,
+      )
+      .then((response) => {
+        setWeather(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching weather data:", error);
+      });
+  }, [country.capital]); // Dependencia para re-ejecutar el efecto si la capital cambia
+
   return (
     <div>
       <h1>{country.name.common}</h1>
@@ -13,6 +30,19 @@ const CountryInformation = ({ country }) => {
         ))}
       </ul>
       <img src={country.flags.png} alt={country.name.common} />
+      <h2>Weather in {country.capital}</h2>
+      <p>
+        {" "}
+        <strong>temperature: </strong> {weather.main.temp}
+      </p>
+      <p>
+        {" "}
+        <strong>description: </strong> {weather.weather[0].description}
+      </p>
+      <p>
+        <strong>wind: </strong>
+        {weather.wind.speed} mps direction {weather.wind.deg} degrees
+      </p>
     </div>
   );
 };

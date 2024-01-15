2248a354604226722b33b72795c6aa53193d8810
Aushzlten12 <josepachas1604@gmail.com>
2024-01-15 11:56:10 -0500

    Exercise 3.16 finished

--- part3/apiPersons/index.js
+++ part3/apiPersons/index.js
@@ -18,10 +18,12 @@ app.use(
   ),
 );
 
-app.get("/api/persons", (request, response) => {
-  Agenda.find({}).then((notes) => {
-    response.json(notes);
-  });
+app.get("/api/persons", (request, response, next) => {
+  Agenda.find({})
+    .then((persons) => {
+      response.json(persons);
+    })
+    .catch((error) => next(error));
 });
 
 // app.get("/info", (request, response) => {
@@ -30,23 +32,31 @@ app.get("/api/persons", (request, response) => {
 //   response.end(`Phonebook has info for ${numpersons} people\n${date}`);
 // });
 
-app.get("/api/persons/:id", (request, response) => {
-  Agenda.findById(request.params.id).then((person) => {
-    response.json(person);
-  });
+app.get("/api/persons/:id", (request, response, next) => {
+  Agenda.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).json({ error: "person not found" });
+      }
+    })
+    .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  Agenda.findByIdAndDelete(request.params.id).then((result) => {
-    if (result) {
-      response.status(204).end();
-    } else {
-      response.status(404).json({ error: "person not found" });
-    }
-  });
+app.delete("/api/persons/:id", (request, response, next) => {
+  Agenda.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      if (result) {
+        response.status(204).end();
+      } else {
+        response.status(404).json({ error: "person not found" });
+      }
+    })
+    .catch((error) => next(error));
 });
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
 
   if (body.name === undefined || body.number === undefined) {
@@ -58,12 +68,32 @@ app.post("/api/persons", (request, response) => {
     number: body.number,
   });
 
-  person.save().then((savedPerson) => {
-    response.json(savedPerson);
-  });
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson);
+    })
+    .catch((error) => next(error));
 });
 
 // error handlers
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 

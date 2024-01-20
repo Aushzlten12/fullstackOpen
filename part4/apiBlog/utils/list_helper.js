c3429bf66d9599fdbbd875bce16b6f80c7266149
Aushzlten12 <josepachas1604@gmail.com>
2024-01-20 18:18:43 -0500

    Exercise 4.6 finished

--- part4/apiBlog/utils/list_helper.js
+++ part4/apiBlog/utils/list_helper.js
@@ -1,3 +1,4 @@
+const _ = require("lodash");
 const dummy = (blogs) => {
   return 1;
 };
@@ -26,4 +27,22 @@ const favoriteBlog = (blogs) => {
   }
 };
 
-module.exports = { dummy, totalLikes, favoriteBlog };
+const mostBlogs = (blogs) => {
+  const authors = _.uniq(blogs.map((blog) => blog.author));
+  const blogsPerAuthor = authors.map((author) => {
+    const blogByAuthor = _.filter(blogs, (blog) => blog.author === author);
+    return {
+      author: author,
+      blogs: blogByAuthor.length,
+    };
+  });
+  blogsPerAuthor.sort(function (a, b) {
+    return b.blogs - a.blogs;
+  });
+  if (blogsPerAuthor.length === 0) {
+    return {};
+  }
+  return blogsPerAuthor[0];
+};
+
+module.exports = { dummy, totalLikes, favoriteBlog, mostBlogs };

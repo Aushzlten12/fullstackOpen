077ae84cca1ffe63b4afe66acd43031f2d874b12
Aushzlten12 <josepachas1604@gmail.com>
2024-01-21 12:16:27 -0500

    Exercise 4.7 finished

--- part4/apiBlog/utils/list_helper.js
+++ part4/apiBlog/utils/list_helper.js
@@ -45,4 +45,19 @@ const mostBlogs = (blogs) => {
   return blogsPerAuthor[0];
 };
 
-module.exports = { dummy, totalLikes, favoriteBlog, mostBlogs };
+const mostLikes = (blogs) => {
+  const groupByAuthor = _.groupBy(blogs, (blog) => blog.author);
+  const groupByAuthorLikes = _.map(groupByAuthor, (blogs) => {
+    const reducer = (sum, blog) => {
+      return sum + blog.likes;
+    };
+    return {
+      author: blogs[0].author,
+      likes: blogs.reduce(reducer, 0),
+    };
+  });
+  const authorpopular = _.orderBy(groupByAuthorLikes, "likes", "desc").shift();
+  return authorpopular || {};
+};
+
+module.exports = { dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes };
